0717e17db57afdfea5f16bfb3f1c80068914089d
Mike Anderson <2572880+mrander@users.noreply.github.com>
2022-01-26 17:24:44 -0500

    Update knps-cli.rb

--- Formula/knps-cli.rb
+++ Formula/knps-cli.rb
@@ -6,7 +6,7 @@ class KnpsCli < Formula
   url "https://files.pythonhosted.org/packages/c5/07/287b1f9fe9028f2b3e1528a78affea255cd0213f874a1b705828136ad369/knps_cli-0.1.0.tar.gz"
   sha256 "a865f0bcf61c2ea1a276cec6d839f752b22aed695284ec70efdac0cf6d7f7d49"
 
-  depends_on "python3"
+  depends_on "python@3.8"
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/ef/75/2bc7bef4d668f9caa9c6ed3f3187989922765403198243040d08d2a52725/setuptools-59.8.0.tar.gz"
